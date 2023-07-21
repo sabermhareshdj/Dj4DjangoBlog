a2fd2830e212320a9e47b5918008c2a38a2c5386
dj.saber.mharesh <dj.saber.mharesh@gmail.com>
2023-07-21 19:09:41 +0300

    class add update delete edit

--- blog/views.py
+++ blog/views.py
@@ -4,53 +4,77 @@ from .forms import PostForm
 # Create your views here.
 
 
+from django.views.generic import ListView , DetailView ,CreateView ,UpdateView ,DeleteView
+
+
+# def post_list(request):
+#     data = Post.objects.all()
+#     return render(request,'post_list.html',{'posts':data})
 
 
 
-def post_list(request):
-
-    data = Post.objects.all()
-
-    return render(request,'post_list.html',{'posts':data})
+class PostList(ListView):
+    model = Post
 
 
-def post_detail(request,post_id):
+# def post_detail(request,post_id):
+#     data = Post.objects.get(id=post_id)
+#     return render(request,'post_detail.html',{'post':data})
 
-    data = Post.objects.get(id=post_id)
-    return render(request,'post_detail.html',{'post':data})
+
+class PostDetail(DetailView):
+    model = Post
 
 
-def post_new(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST,request.FILES)
-        if form.is_valid():
-            myform = form.save(commit = False)
-            myform.author = request.user
-            myform.save()
-            return redirect('/blog')
-    else: 
-        form = PostForm()
+# def post_new(request):
+#     if request.method == 'POST':
+#         form = PostForm(request.POST,request.FILES)
+#         if form.is_valid():
+#             myform = form.save(commit = False)
+#             myform.author = request.user
+#             myform.save()
+#             return redirect('/blog')
+#     else: 
+#         form = PostForm()
 
-    return render(request,'new_post.html',{'form':form})
+#     return render(request,'new_post.html',{'form':form})
 
-def edit_post(request,post_id):
-     data = Post.objects.get(id=post_id)
-     if request.method == 'POST':
-        form = PostForm(request.POST,request.FILES,instance=data)
-        if form.is_valid():
-            myform = form.save(commit = False)
-            myform.author = request.user
-            myform.save()
-            return redirect('/blog')
+
+class PostCreate(CreateView):
+    model = Post
+    fields = [ 'title','content','create_date','draft','tags','author','image']
+    success_url = '/blog'
+
+
+# def edit_post(request,post_id):
+#      data = Post.objects.get(id=post_id)
+#      if request.method == 'POST':
+#         form = PostForm(request.POST,request.FILES,instance=data)
+#         if form.is_valid():
+#             myform = form.save(commit = False)
+#             myform.author = request.user
+#             myform.save()
+#             return redirect('/blog')
         
-     else: 
-        form = PostForm(instance=data)
+#      else: 
+#         form = PostForm(instance=data)
 
-     return render(request,'edit_post.html',{'form':form})
+#      return render(request,'edit_post.html',{'form':form})
 
 
+class PostUpdate(UpdateView):
+    model = Post
+    fields = [ 'title','content','create_date','draft','tags','author','image']
+    success_url = '/blog'
+    template_name = 'blog\edit_post.html'
 
-def delete_post(request,post_id):
-    data = Post.objects.get(id=post_id)
-    data.delete()
-    return redirect('/blog')
+
+# def delete_post(request,post_id):
+#     data = Post.objects.get(id=post_id)
+#     data.delete()
+#     return redirect('/blog')
+
+
+class PostDelete(DeleteView):
+    model = Post
+    success_url = '/blog'
