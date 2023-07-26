aca2888e66318d743d2ebdbe97ef56b4c2709191
dj.saber.mharesh <dj.saber.mharesh@gmail.com>
2023-07-26 12:43:19 +0300

    add summernote1

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,9 @@ from .models import Post,Comment
 
 class PostAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
     summernote_fields = '__all__'
-
+    list_display = ['title','draft','author']
+    list_filter = ['title','draft']
+    search_fields = ['title','draft']
 admin.site.register(Post,PostAdmin)
 admin.site.register(Comment)
 
