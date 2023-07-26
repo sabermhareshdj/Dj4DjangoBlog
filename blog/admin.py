e6f0da5873295168d0911e1abc3a85fe24381b83
dj.saber.mharesh <dj.saber.mharesh@gmail.com>
2023-07-26 11:37:34 +0300

    add summernote

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-
+from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 from .models import Post,Comment
 
-admin.site.register(Post)
+class PostAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
+    summernote_fields = '__all__'
+
+admin.site.register(Post,PostAdmin)
 admin.site.register(Comment)
 
--- project/urls.py
+++ project/urls.py
@@ -14,14 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.contrib import admin 
+    
+from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
 from blog.views import PostList , post_detail,PostCreate,PostUpdate,PostDelete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path('blog/' , PostList.as_view()),
     path('blog/new' , PostCreate.as_view()),
     path('blog/<int:pk>' , post_detail),
